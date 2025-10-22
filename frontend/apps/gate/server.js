7839ba19fe3e88e4df8c96b913451aebb74cd5bc
Brion Mario <brionbmp@gmail.com>
2025-10-22 11:31:59 +0530

    Update Host

--- frontend/apps/gate/server.js
+++ frontend/apps/gate/server.js
@@ -37,7 +37,7 @@ if (fs.existsSync(requiredServerFilesConfig)) {
 }
 
 const PORT = process.env.PORT || 9090;
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || '0.0.0.0';
 const ENABLE_HTTPS = process.env.ENABLE_HTTPS !== 'false';
 const keyPath = path.resolve(__dirname, 'server.key');
 const certPath = path.resolve(__dirname, 'server.cert');
