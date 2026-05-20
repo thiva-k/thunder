2470da8b5d88d6cb0bf5ce11e6c853efd8e3d442
Jerad Rutnam <jeradrutnam@outlook.com>
2026-05-20 14:21:36 +0530

    Improve docs home page

--- docs/src/theme/MDXComponents.tsx
+++ docs/src/theme/MDXComponents.tsx
@@ -16,10 +16,13 @@
  * under the License.
  */
 
+import MDXComponents from '@theme-original/MDXComponents';
+import {Box, Card, CardContent, Typography, ColorSchemeSVG} from '@wso2/oxygen-ui';
 import ApiReference from '@site/src/components/ApiReference';
 import CodeBlock from '@site/src/components/CodeBlock';
-import DocsGetStarted from '@site/src/components/DocsGetStarted';
 import CodeGroup from '@site/src/components/CodeGroup';
+import ColorSchemeImage from '@site/src/components/ColorSchemeImage';
+import DocsGetStarted from '@site/src/components/DocsGetStarted';
 import FloatingLogosBackground from '@site/src/components/FloatingLogosBackground';
 import AndroidLogo from '@site/src/components/icons/AndroidLogo';
 import AngularLogo from '@site/src/components/icons/AngularLogo';
@@ -40,8 +43,6 @@ import RepoLink from '@site/src/components/RepoLink';
 import SDKCard from '@site/src/components/SDKCard';
 import Stepper from '@site/src/components/Stepper';
 import TutorialHero, {TutorialHeroItem} from '@site/src/components/TutorialHero';
-import MDXComponents from '@theme-original/MDXComponents';
-import {Box, Card, CardContent, Typography, ColorSchemeSVG} from '@wso2/oxygen-ui';
 
 export default {
   ...MDXComponents,
@@ -49,6 +50,7 @@ export default {
   Card,
   CardContent,
   ColorSchemeSVG,
+  ColorSchemeImage,
   Typography,
   DocsGetStarted,
   Stepper,
