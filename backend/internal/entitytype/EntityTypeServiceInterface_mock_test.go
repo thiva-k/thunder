thunder-id/thunderid

Add endpoint to retrieve user's schema metadata

backend/internal/entitytype/EntityTypeServiceInterface_mock_test.go
@@ -596,6 +596,82 @@ func (_c *EntityTypeServiceInterfaceMock_GetEntityTypeList_Call) RunAndReturn(ru
 	return _c
 }
 
+// GetSelfUserTypeSchema provides a mock function for the type EntityTypeServiceInterfaceMock
+func (_mock *EntityTypeServiceInterfaceMock) GetSelfUserTypeSchema(ctx context.Context, category TypeCategory, userTypeName string) (*EntityType, *common.ServiceError) {
+	ret := _mock.Called(ctx, category, userTypeName)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetSelfUserTypeSchema")
+	}
+
+	var r0 *EntityType
+	var r1 *common.ServiceError
+	if returnFunc, ok := ret.Get(0).(func(context.Context, TypeCategory, string) (*EntityType, *common.ServiceError)); ok {
+		return returnFunc(ctx, category, userTypeName)
+	}
+	if returnFunc, ok := ret.Get(0).(func(context.Context, TypeCategory, string) *EntityType); ok {
+		r0 = returnFunc(ctx, category, userTypeName)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*EntityType)
+		}
+	}
+	if returnFunc, ok := ret.Get(1).(func(context.Context, TypeCategory, string) *common.ServiceError); ok {
+		r1 = returnFunc(ctx, category, userTypeName)
+	} else {
+		if ret.Get(1) != nil {
+			r1 = ret.Get(1).(*common.ServiceError)
+		}
+	}
+	return r0, r1
+}
+
+// EntityTypeServiceInterfaceMock_GetSelfUserTypeSchema_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GetSelfUserTypeSchema'
+type EntityTypeServiceInterfaceMock_GetSelfUserTypeSchema_Call struct {
+	*mock.Call
+}
+
+// GetSelfUserTypeSchema is a helper method to define mock.On call
+//   - ctx context.Context
+//   - category TypeCategory
+//   - userTypeName string
+func (_e *EntityTypeServiceInterfaceMock_Expecter) GetSelfUserTypeSchema(ctx interface{}, category interface{}, userTypeName interface{}) *EntityTypeServiceInterfaceMock_GetSelfUserTypeSchema_Call {
+	return &EntityTypeServiceInterfaceMock_GetSelfUserTypeSchema_Call{Call: _e.mock.On("GetSelfUserTypeSchema", ctx, category, userTypeName)}
+}
+
+func (_c *EntityTypeServiceInterfaceMock_GetSelfUserTypeSchema_Call) Run(run func(ctx context.Context, category TypeCategory, userTypeName string)) *EntityTypeServiceInterfaceMock_GetSelfUserTypeSchema_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		var arg0 context.Context
+		if args[0] != nil {
+			arg0 = args[0].(context.Context)
+		}
+		var arg1 TypeCategory
+		if args[1] != nil {
+			arg1 = args[1].(TypeCategory)
+		}
+		var arg2 string
+		if args[2] != nil {
+			arg2 = args[2].(string)
+		}
+		run(
+			arg0,
+			arg1,
+			arg2,
+		)
+	})
+	return _c
+}
+
+func (_c *EntityTypeServiceInterfaceMock_GetSelfUserTypeSchema_Call) Return(entityType *EntityType, serviceError *common.ServiceError) *EntityTypeServiceInterfaceMock_GetSelfUserTypeSchema_Call {
+	_c.Call.Return(entityType, serviceError)
+	return _c
+}
+
+func (_c *EntityTypeServiceInterfaceMock_GetSelfUserTypeSchema_Call) RunAndReturn(run func(ctx context.Context, category TypeCategory, userTypeName string) (*EntityType, *common.ServiceError)) *EntityTypeServiceInterfaceMock_GetSelfUserTypeSchema_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // GetUniqueAttributes provides a mock function for the type EntityTypeServiceInterfaceMock
 func (_mock *EntityTypeServiceInterfaceMock) GetUniqueAttributes(ctx context.Context, category TypeCategory, entityType string) ([]string, *common.ServiceError) {
 	ret := _mock.Called(ctx, category, entityType)

backend/internal/entitytype/handler_test.go
@@ -112,6 +112,12 @@ func (s *InlineStubEntityTypeService) ValidateEntityUniqueness(
 	return true, nil
 }
 
+func (s *InlineStubEntityTypeService) GetSelfUserTypeSchema(
+	ctx context.Context, cat TypeCategory, name string,
+) (*EntityType, *tidcommon.ServiceError) {
+	return &EntityType{Name: name, Category: cat}, nil
+}
+
 // --- POST ENDPOINT TESTS ---
 
 func TestHandleEntityTypePostRequest_Success(t *testing.T) {

backend/internal/entitytype/service.go
@@ -84,6 +84,9 @@ type EntityTypeServiceInterface interface {
 		ctx context.Context, category TypeCategory, names []string,
 	) (map[string]string, *tidcommon.ServiceError)
 	ResolveEntityTypeHandles(ctx context.Context, entityType *EntityType) *tidcommon.ServiceError
+	GetSelfUserTypeSchema(
+		ctx context.Context, category TypeCategory, userTypeName string,
+	) (*EntityType, *tidcommon.ServiceError)
 }
 
 // entityTypeService is the default implementation of the EntityTypeServiceInterface.
@@ -322,6 +325,31 @@ func (us *entityTypeService) GetEntityType(
 		} else if handle, ok := handleMap[entityType.OUID]; ok {
 			entityType.OUHandle = handle
 		}
+	}
+
+	return &entityType, nil
+}
+
+// GetSelfUserTypeSchema retrieves the schema config by name without admin access check
+func (us *entityTypeService) GetSelfUserTypeSchema(
+	ctx context.Context, category TypeCategory, userTypeName string,
+) (*EntityType, *tidcommon.ServiceError) {
+	logger := log.GetLogger().With(log.String(log.LoggerKeyComponentName, entityTypeLoggerComponentName))
+
+	if svcErr := validateCategory(category); svcErr != nil {
+		return nil, svcErr
+	}
+
+	if userTypeName == "" {
+		return nil, invalidEntityTypeRequestErr(category, "schema name must not be empty")
+	}
+
+	entityType, err := us.entityTypeStore.GetEntityTypeByName(ctx, category, userTypeName)
+	if err != nil {
+		if errors.Is(err, ErrEntityTypeNotFound) {
+			return nil, entityTypeNotFoundErr(category)
+		}
+		return nil, logAndReturnServerError(ctx, logger, "Failed to get entity type", err)
 	}
 
 	return &entityType, nil

backend/internal/entitytype/service_test.go
@@ -1916,6 +1916,67 @@ func (s *EntityTypeServiceTestSuite) TestGetAttributes_NonCredential_StoreError_
 	s.Require().Nil(attrs)
 }
 
+func (s *EntityTypeServiceTestSuite) TestGetSelfUserTypeSchema_Success() {
+	storeMock := newEntityTypeStoreInterfaceMock(s.T())
+	storeMock.
+		On("GetEntityTypeByName", context.Background(), TypeCategoryUser, "employee").
+		Return(EntityType{Name: "employee", Schema: json.RawMessage(`{}`)}, nil).
+		Once()
+
+	service := &entityTypeService{entityTypeStore: storeMock}
+
+	entityType, svcErr := service.GetSelfUserTypeSchema(context.Background(), TypeCategoryUser, "employee")
+	s.Require().Nil(svcErr)
+	s.Require().NotNil(entityType)
+	s.Require().Equal("employee", entityType.Name)
+}
+
+func (s *EntityTypeServiceTestSuite) TestGetSelfUserTypeSchema_InvalidCategory() {
+	service := &entityTypeService{}
+	entityType, svcErr := service.GetSelfUserTypeSchema(context.Background(), TypeCategory("invalid"), "employee")
+	s.Require().Nil(entityType)
+	s.Require().NotNil(svcErr)
+	s.Require().Equal(ErrorInvalidEntityTypeRequest.Code, svcErr.Code)
+}
+
+func (s *EntityTypeServiceTestSuite) TestGetSelfUserTypeSchema_EmptyName() {
+	service := &entityTypeService{}
+	entityType, svcErr := service.GetSelfUserTypeSchema(context.Background(), TypeCategoryUser, "")
+	s.Require().Nil(entityType)
+	s.Require().NotNil(svcErr)
+	s.Require().Equal(ErrorInvalidEntityTypeRequest.Code, svcErr.Code)
+}
+
+func (s *EntityTypeServiceTestSuite) TestGetSelfUserTypeSchema_NotFound() {
+	storeMock := newEntityTypeStoreInterfaceMock(s.T())
+	storeMock.
+		On("GetEntityTypeByName", context.Background(), TypeCategoryUser, "employee").
+		Return(EntityType{}, ErrEntityTypeNotFound).
+		Once()
+
+	service := &entityTypeService{entityTypeStore: storeMock}
+
+	entityType, svcErr := service.GetSelfUserTypeSchema(context.Background(), TypeCategoryUser, "employee")
+	s.Require().Nil(entityType)
+	s.Require().NotNil(svcErr)
+	s.Require().Equal(ErrorEntityTypeNotFound.Code, svcErr.Code)
+}
+
+func (s *EntityTypeServiceTestSuite) TestGetSelfUserTypeSchema_StoreError() {
+	storeMock := newEntityTypeStoreInterfaceMock(s.T())
+	storeMock.
+		On("GetEntityTypeByName", context.Background(), TypeCategoryUser, "employee").
+		Return(EntityType{}, errors.New("db error")).
+		Once()
+
+	service := &entityTypeService{entityTypeStore: storeMock}
+
+	entityType, svcErr := service.GetSelfUserTypeSchema(context.Background(), TypeCategoryUser, "employee")
+	s.Require().Nil(entityType)
+	s.Require().NotNil(svcErr)
+	s.Require().Equal(tidcommon.InternalServerError, *svcErr)
+}
+
 // TestGetCompiledSchemaForEntityType_CompileError verifies that a stored schema which fails to
 // compile surfaces as an internal server error through ValidateEntity.
 func TestGetCompiledSchemaForEntityType_CompileError(t *testing.T) {

backend/internal/user/service.go
@@ -59,6 +59,7 @@ type UserServiceInterface interface {
 	UpdateUser(ctx context.Context, userID string, user *User) (*User, *tidcommon.ServiceError)
 	UpdateUserAttributes(ctx context.Context, userID string,
 		attributes json.RawMessage) (*User, *tidcommon.ServiceError)
+	GetUserMetadata(ctx context.Context, userID string) (*entitytype.EntityType, *tidcommon.ServiceError)
 	UpdateUserCredentials(ctx context.Context, userID string,
 		credentials json.RawMessage) *tidcommon.ServiceError
 	DeleteUser(ctx context.Context, userID string) *tidcommon.ServiceError
@@ -434,6 +435,23 @@ func (us *userService) GetUser(
 
 	logger.Debug(ctx, "Successfully retrieved user", log.MaskedString(log.LoggerKeyUserID, userID))
 	return &user, nil
+}
+
+// GetUserMetadata retrieves the schema metadata for the authenticated user's type.
+func (us *userService) GetUserMetadata(
+	ctx context.Context, userID string,
+) (*entitytype.EntityType, *tidcommon.ServiceError) {
+	user, svcErr := us.GetUser(ctx, userID, false)
+	if svcErr != nil {
+		return nil, svcErr
+	}
+
+	schema, svcErr := us.entityTypeService.GetSelfUserTypeSchema(ctx, entitytype.TypeCategoryUser, user.Type)
+	if svcErr != nil {
+		return nil, svcErr
+	}
+
+	return schema, nil
 }
 
 // GetUserGroups retrieves groups of a user with pagination.

backend/internal/user/service_test.go
@@ -3844,3 +3844,77 @@ func TestUserService_GetUserUsages_RegistryError(t *testing.T) {
 	require.Nil(t, result)
 	require.NotNil(t, err)
 }
+
+func TestGetUserMetadata_Success(t *testing.T) {
+	entityTypeMock := entitytypemock.NewEntityTypeServiceInterfaceMock(t)
+	expectedSchema := &entitytype.EntityType{
+		Name:   testUserType,
+		Schema: json.RawMessage(`{"email":{"type":"string"}}`),
+	}
+	entityTypeMock.On("GetSelfUserTypeSchema", mock.Anything, entitytype.TypeCategoryUser, testUserType).
+		Return(expectedSchema, (*tidcommon.ServiceError)(nil)).Once()
+
+	entityServiceMock := entitymock.NewEntityServiceInterfaceMock(t)
+	entityUser := &providers.Entity{
+		ID:         svcTestUserID123,
+		Category:   providers.EntityCategoryUser,
+		Type:       testUserType,
+		Attributes: json.RawMessage(`{"username":"test"}`),
+	}
+	entityServiceMock.On("GetEntity", mock.Anything, svcTestUserID123).
+		Return(entityUser, nil).Once()
+
+	service := &userService{
+		entityService:     entityServiceMock,
+		entityTypeService: entityTypeMock,
+		authzService:      newAllowAllAuthz(t),
+	}
+
+	schema, svcErr := service.GetUserMetadata(context.Background(), svcTestUserID123)
+	require.Nil(t, svcErr)
+	require.NotNil(t, schema)
+	require.Equal(t, testUserType, schema.Name)
+}
+
+func TestGetUserMetadata_GetUserError(t *testing.T) {
+	entityServiceMock := entitymock.NewEntityServiceInterfaceMock(t)
+	entityServiceMock.On("GetEntity", mock.Anything, svcTestUserID123).
+		Return((*providers.Entity)(nil), entitypkg.ErrEntityNotFound).Once()
+
+	service := &userService{
+		entityService: entityServiceMock,
+		authzService:  newAllowAllAuthz(t),
+	}
+
+	schema, svcErr := service.GetUserMetadata(context.Background(), svcTestUserID123)
+	require.Nil(t, schema)
+	require.NotNil(t, svcErr)
+	require.Equal(t, ErrorUserNotFound.Code, svcErr.Code)
+}
+
+func TestGetUserMetadata_GetSelfUserTypeSchemaError(t *testing.T) {
+	entityServiceMock := entitymock.NewEntityServiceInterfaceMock(t)
+	entityUser := &providers.Entity{
+		ID:         svcTestUserID123,
+		Category:   providers.EntityCategoryUser,
+		Type:       testUserType,
+		Attributes: json.RawMessage(`{"username":"test"}`),
+	}
+	entityServiceMock.On("GetEntity", mock.Anything, svcTestUserID123).
+		Return(entityUser, nil).Once()
+
+	entityTypeMock := entitytypemock.NewEntityTypeServiceInterfaceMock(t)
+	entityTypeMock.On("GetSelfUserTypeSchema", mock.Anything, entitytype.TypeCategoryUser, testUserType).
+		Return((*entitytype.EntityType)(nil), &entitytype.ErrorEntityTypeNotFound).Once()
+
+	service := &userService{
+		entityService:     entityServiceMock,
+		entityTypeService: entityTypeMock,
+		authzService:      newAllowAllAuthz(t),
+	}
+
+	schema, svcErr := service.GetUserMetadata(context.Background(), svcTestUserID123)
+	require.Nil(t, schema)
+	require.NotNil(t, svcErr)
+	require.Equal(t, entitytype.ErrorEntityTypeNotFound.Code, svcErr.Code)
+}

thiva-k/thunder

backend/internal/entitytype/EntityTypeServiceInterface_mock_test.go
@@ -596,6 +596,82 @@ func (_c *EntityTypeServiceInterfaceMock_GetEntityTypeList_Call) RunAndReturn(ru
 	return _c
 }
 
+// GetEntityTypeSchema provides a mock function for the type EntityTypeServiceInterfaceMock
+func (_mock *EntityTypeServiceInterfaceMock) GetEntityTypeSchema(ctx context.Context, category TypeCategory, userTypeName string) (*EntityType, *common.ServiceError) {
+	ret := _mock.Called(ctx, category, userTypeName)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetEntityTypeSchema")
+	}
+
+	var r0 *EntityType
+	var r1 *common.ServiceError
+	if returnFunc, ok := ret.Get(0).(func(context.Context, TypeCategory, string) (*EntityType, *common.ServiceError)); ok {
+		return returnFunc(ctx, category, userTypeName)
+	}
+	if returnFunc, ok := ret.Get(0).(func(context.Context, TypeCategory, string) *EntityType); ok {
+		r0 = returnFunc(ctx, category, userTypeName)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*EntityType)
+		}
+	}
+	if returnFunc, ok := ret.Get(1).(func(context.Context, TypeCategory, string) *common.ServiceError); ok {
+		r1 = returnFunc(ctx, category, userTypeName)
+	} else {
+		if ret.Get(1) != nil {
+			r1 = ret.Get(1).(*common.ServiceError)
+		}
+	}
+	return r0, r1
+}
+
+// EntityTypeServiceInterfaceMock_GetEntityTypeSchema_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GetEntityTypeSchema'
+type EntityTypeServiceInterfaceMock_GetEntityTypeSchema_Call struct {
+	*mock.Call
+}
+
+// GetEntityTypeSchema is a helper method to define mock.On call
+//   - ctx context.Context
+//   - category TypeCategory
+//   - userTypeName string
+func (_e *EntityTypeServiceInterfaceMock_Expecter) GetEntityTypeSchema(ctx interface{}, category interface{}, userTypeName interface{}) *EntityTypeServiceInterfaceMock_GetEntityTypeSchema_Call {
+	return &EntityTypeServiceInterfaceMock_GetEntityTypeSchema_Call{Call: _e.mock.On("GetEntityTypeSchema", ctx, category, userTypeName)}
+}
+
+func (_c *EntityTypeServiceInterfaceMock_GetEntityTypeSchema_Call) Run(run func(ctx context.Context, category TypeCategory, userTypeName string)) *EntityTypeServiceInterfaceMock_GetEntityTypeSchema_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		var arg0 context.Context
+		if args[0] != nil {
+			arg0 = args[0].(context.Context)
+		}
+		var arg1 TypeCategory
+		if args[1] != nil {
+			arg1 = args[1].(TypeCategory)
+		}
+		var arg2 string
+		if args[2] != nil {
+			arg2 = args[2].(string)
+		}
+		run(
+			arg0,
+			arg1,
+			arg2,
+		)
+	})
+	return _c
+}
+
+func (_c *EntityTypeServiceInterfaceMock_GetEntityTypeSchema_Call) Return(entityType *EntityType, serviceError *common.ServiceError) *EntityTypeServiceInterfaceMock_GetEntityTypeSchema_Call {
+	_c.Call.Return(entityType, serviceError)
+	return _c
+}
+
+func (_c *EntityTypeServiceInterfaceMock_GetEntityTypeSchema_Call) RunAndReturn(run func(ctx context.Context, category TypeCategory, userTypeName string) (*EntityType, *common.ServiceError)) *EntityTypeServiceInterfaceMock_GetEntityTypeSchema_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // GetUniqueAttributes provides a mock function for the type EntityTypeServiceInterfaceMock
 func (_mock *EntityTypeServiceInterfaceMock) GetUniqueAttributes(ctx context.Context, category TypeCategory, entityType string) ([]string, *common.ServiceError) {
 	ret := _mock.Called(ctx, category, entityType)

backend/internal/entitytype/handler_test.go
@@ -112,6 +112,12 @@ func (s *InlineStubEntityTypeService) ValidateEntityUniqueness(
 	return true, nil
 }
 
+func (s *InlineStubEntityTypeService) GetEntityTypeSchema(
+	ctx context.Context, cat TypeCategory, name string,
+) (*EntityType, *tidcommon.ServiceError) {
+	return &EntityType{Name: name, Category: cat}, nil
+}
+
 // --- POST ENDPOINT TESTS ---
 
 func TestHandleEntityTypePostRequest_Success(t *testing.T) {

backend/internal/entitytype/service.go
@@ -84,6 +84,9 @@ type EntityTypeServiceInterface interface {
 		ctx context.Context, category TypeCategory, names []string,
 	) (map[string]string, *tidcommon.ServiceError)
 	ResolveEntityTypeHandles(ctx context.Context, entityType *EntityType) *tidcommon.ServiceError
+	GetEntityTypeSchema(
+		ctx context.Context, category TypeCategory, userTypeName string,
+	) (*EntityType, *tidcommon.ServiceError)
 }
 
 // entityTypeService is the default implementation of the EntityTypeServiceInterface.
@@ -322,6 +325,31 @@ func (us *entityTypeService) GetEntityType(
 		} else if handle, ok := handleMap[entityType.OUID]; ok {
 			entityType.OUHandle = handle
 		}
+	}
+
+	return &entityType, nil
+}
+
+// GetEntityTypeSchema retrieves the schema config by name without admin access check
+func (us *entityTypeService) GetEntityTypeSchema(
+	ctx context.Context, category TypeCategory, userTypeName string,
+) (*EntityType, *tidcommon.ServiceError) {
+	logger := log.GetLogger().With(log.String(log.LoggerKeyComponentName, entityTypeLoggerComponentName))
+
+	if svcErr := validateCategory(category); svcErr != nil {
+		return nil, svcErr
+	}
+
+	if userTypeName == "" {
+		return nil, invalidEntityTypeRequestErr(category, "schema name must not be empty")
+	}
+
+	entityType, err := us.entityTypeStore.GetEntityTypeByName(ctx, category, userTypeName)
+	if err != nil {
+		if errors.Is(err, ErrEntityTypeNotFound) {
+			return nil, entityTypeNotFoundErr(category)
+		}
+		return nil, logAndReturnServerError(ctx, logger, "Failed to get entity type", err)
 	}
 
 	return &entityType, nil

backend/internal/entitytype/service_test.go
@@ -1916,6 +1916,67 @@ func (s *EntityTypeServiceTestSuite) TestGetAttributes_NonCredential_StoreError_
 	s.Require().Nil(attrs)
 }
 
+func (s *EntityTypeServiceTestSuite) TestGetEntityTypeSchema_Success() {
+	storeMock := newEntityTypeStoreInterfaceMock(s.T())
+	storeMock.
+		On("GetEntityTypeByName", context.Background(), TypeCategoryUser, "employee").
+		Return(EntityType{Name: "employee", Schema: json.RawMessage(`{}`)}, nil).
+		Once()
+
+	service := &entityTypeService{entityTypeStore: storeMock}
+
+	entityType, svcErr := service.GetEntityTypeSchema(context.Background(), TypeCategoryUser, "employee")
+	s.Require().Nil(svcErr)
+	s.Require().NotNil(entityType)
+	s.Require().Equal("employee", entityType.Name)
+}
+
+func (s *EntityTypeServiceTestSuite) TestGetEntityTypeSchema_InvalidCategory() {
+	service := &entityTypeService{}
+	entityType, svcErr := service.GetEntityTypeSchema(context.Background(), TypeCategory("invalid"), "employee")
+	s.Require().Nil(entityType)
+	s.Require().NotNil(svcErr)
+	s.Require().Equal(ErrorInvalidEntityTypeRequest.Code, svcErr.Code)
+}
+
+func (s *EntityTypeServiceTestSuite) TestGetEntityTypeSchema_EmptyName() {
+	service := &entityTypeService{}
+	entityType, svcErr := service.GetEntityTypeSchema(context.Background(), TypeCategoryUser, "")
+	s.Require().Nil(entityType)
+	s.Require().NotNil(svcErr)
+	s.Require().Equal(ErrorInvalidEntityTypeRequest.Code, svcErr.Code)
+}
+
+func (s *EntityTypeServiceTestSuite) TestGetEntityTypeSchema_NotFound() {
+	storeMock := newEntityTypeStoreInterfaceMock(s.T())
+	storeMock.
+		On("GetEntityTypeByName", context.Background(), TypeCategoryUser, "employee").
+		Return(EntityType{}, ErrEntityTypeNotFound).
+		Once()
+
+	service := &entityTypeService{entityTypeStore: storeMock}
+
+	entityType, svcErr := service.GetEntityTypeSchema(context.Background(), TypeCategoryUser, "employee")
+	s.Require().Nil(entityType)
+	s.Require().NotNil(svcErr)
+	s.Require().Equal(ErrorEntityTypeNotFound.Code, svcErr.Code)
+}
+
+func (s *EntityTypeServiceTestSuite) TestGetEntityTypeSchema_StoreError() {
+	storeMock := newEntityTypeStoreInterfaceMock(s.T())
+	storeMock.
+		On("GetEntityTypeByName", context.Background(), TypeCategoryUser, "employee").
+		Return(EntityType{}, errors.New("db error")).
+		Once()
+
+	service := &entityTypeService{entityTypeStore: storeMock}
+
+	entityType, svcErr := service.GetEntityTypeSchema(context.Background(), TypeCategoryUser, "employee")
+	s.Require().Nil(entityType)
+	s.Require().NotNil(svcErr)
+	s.Require().Equal(tidcommon.InternalServerError, *svcErr)
+}
+
 // TestGetCompiledSchemaForEntityType_CompileError verifies that a stored schema which fails to
 // compile surfaces as an internal server error through ValidateEntity.
 func TestGetCompiledSchemaForEntityType_CompileError(t *testing.T) {

backend/internal/user/service.go
@@ -59,6 +59,7 @@ type UserServiceInterface interface {
 	UpdateUser(ctx context.Context, userID string, user *User) (*User, *tidcommon.ServiceError)
 	UpdateUserAttributes(ctx context.Context, userID string,
 		attributes json.RawMessage) (*User, *tidcommon.ServiceError)
+	GetUserMetadata(ctx context.Context, userID string) (*entitytype.EntityType, *tidcommon.ServiceError)
 	UpdateUserCredentials(ctx context.Context, userID string,
 		credentials json.RawMessage) *tidcommon.ServiceError
 	DeleteUser(ctx context.Context, userID string) *tidcommon.ServiceError
@@ -434,6 +435,23 @@ func (us *userService) GetUser(
 
 	logger.Debug(ctx, "Successfully retrieved user", log.MaskedString(log.LoggerKeyUserID, userID))
 	return &user, nil
+}
+
+// GetUserMetadata retrieves the schema metadata for the authenticated user's type.
+func (us *userService) GetUserMetadata(
+	ctx context.Context, userID string,
+) (*entitytype.EntityType, *tidcommon.ServiceError) {
+	user, svcErr := us.GetUser(ctx, userID, false)
+	if svcErr != nil {
+		return nil, svcErr
+	}
+
+	schema, svcErr := us.entityTypeService.GetEntityTypeSchema(ctx, entitytype.TypeCategoryUser, user.Type)
+	if svcErr != nil {
+		return nil, svcErr
+	}
+
+	return schema, nil
 }
 
 // GetUserGroups retrieves groups of a user with pagination.

backend/internal/user/service_test.go
@@ -3844,3 +3844,77 @@ func TestUserService_GetUserUsages_RegistryError(t *testing.T) {
 	require.Nil(t, result)
 	require.NotNil(t, err)
 }
+
+func TestGetUserMetadata_Success(t *testing.T) {
+	entityTypeMock := entitytypemock.NewEntityTypeServiceInterfaceMock(t)
+	expectedSchema := &entitytype.EntityType{
+		Name:   testUserType,
+		Schema: json.RawMessage(`{"email":{"type":"string"}}`),
+	}
+	entityTypeMock.On("GetEntityTypeSchema", mock.Anything, entitytype.TypeCategoryUser, testUserType).
+		Return(expectedSchema, (*tidcommon.ServiceError)(nil)).Once()
+
+	entityServiceMock := entitymock.NewEntityServiceInterfaceMock(t)
+	entityUser := &providers.Entity{
+		ID:         svcTestUserID123,
+		Category:   providers.EntityCategoryUser,
+		Type:       testUserType,
+		Attributes: json.RawMessage(`{"username":"test"}`),
+	}
+	entityServiceMock.On("GetEntity", mock.Anything, svcTestUserID123).
+		Return(entityUser, nil).Once()
+
+	service := &userService{
+		entityService:     entityServiceMock,
+		entityTypeService: entityTypeMock,
+		authzService:      newAllowAllAuthz(t),
+	}
+
+	schema, svcErr := service.GetUserMetadata(context.Background(), svcTestUserID123)
+	require.Nil(t, svcErr)
+	require.NotNil(t, schema)
+	require.Equal(t, testUserType, schema.Name)
+}
+
+func TestGetUserMetadata_GetUserError(t *testing.T) {
+	entityServiceMock := entitymock.NewEntityServiceInterfaceMock(t)
+	entityServiceMock.On("GetEntity", mock.Anything, svcTestUserID123).
+		Return((*providers.Entity)(nil), entitypkg.ErrEntityNotFound).Once()
+
+	service := &userService{
+		entityService: entityServiceMock,
+		authzService:  newAllowAllAuthz(t),
+	}
+
+	schema, svcErr := service.GetUserMetadata(context.Background(), svcTestUserID123)
+	require.Nil(t, schema)
+	require.NotNil(t, svcErr)
+	require.Equal(t, ErrorUserNotFound.Code, svcErr.Code)
+}
+
+func TestGetUserMetadata_GetEntityTypeSchemaError(t *testing.T) {
+	entityServiceMock := entitymock.NewEntityServiceInterfaceMock(t)
+	entityUser := &providers.Entity{
+		ID:         svcTestUserID123,
+		Category:   providers.EntityCategoryUser,
+		Type:       testUserType,
+		Attributes: json.RawMessage(`{"username":"test"}`),
+	}
+	entityServiceMock.On("GetEntity", mock.Anything, svcTestUserID123).
+		Return(entityUser, nil).Once()
+
+	entityTypeMock := entitytypemock.NewEntityTypeServiceInterfaceMock(t)
+	entityTypeMock.On("GetEntityTypeSchema", mock.Anything, entitytype.TypeCategoryUser, testUserType).
+		Return((*entitytype.EntityType)(nil), &entitytype.ErrorEntityTypeNotFound).Once()
+
+	service := &userService{
+		entityService:     entityServiceMock,
+		entityTypeService: entityTypeMock,
+		authzService:      newAllowAllAuthz(t),
+	}
+
+	schema, svcErr := service.GetUserMetadata(context.Background(), svcTestUserID123)
+	require.Nil(t, schema)
+	require.NotNil(t, svcErr)
+	require.Equal(t, entitytype.ErrorEntityTypeNotFound.Code, svcErr.Code)
+}